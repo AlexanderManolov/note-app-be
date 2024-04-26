ae2a4c4bb39f171cdcbf4bc2d8c8d2f57a90f29c
alexander.manolov <alexander.manolov@ableto.com>
2024-04-26 11:30:38 +0300

    Add prettier, eslint, and npmrc.

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,15 @@ configDotenv()
 const PORT = process.env.PORT ?? '5050'
 const app = express()
 
-app.get("/", (request: Request, response: Response) => { 
-  response.status(200).send("Hello World")
-}) 
-
-app.listen(PORT, () => { 
-  console.log("Server running at PORT: ", PORT) 
-}).on("error", (error: Error) => {
-  // gracefully handle error
-  throw new Error(error.message)
+app.get('/', (request: Request, response: Response) => {
+	response.status(200).send('Hello World')
 })
+
+app
+	.listen(PORT, () => {
+		console.log('Server running at PORT: ', PORT)
+	})
+	.on('error', (error: Error) => {
+		// gracefully handle error
+		throw new Error(error.message)
+	})
